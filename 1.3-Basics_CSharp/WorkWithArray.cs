93c0e66ecceee9c5386b65cb9d5ea2a9ef2a4696
Joffrey Kern <joffrey.kern@live.fr>
2012-10-16 09:47:38 +0200

    add arrays

---
+++ 1.3-Basics_CSharp/WorkWithArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3_Basics_CSharp
+{
+    class WorkWithArray
+    {
+        int[] firstarray;
+        int[] initarray = new int[3] { 1, 2, 3 };
+
+        void AddValues()
+        {
+            firstarray = new int[3];
+            firstarray[0] = 1;
+            firstarray[1] = 2;
+            firstarray[2] = 3;
+        }
+    }
+}
