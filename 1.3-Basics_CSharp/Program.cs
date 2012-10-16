0fffa649063d1077e32a7396d741d683e6ec3800
Joffrey Kern <joffrey.kern@live.fr>
2012-10-16 09:38:53 +0200

    add enum

---
+++ 1.3-Basics_CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3_Basics_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    enum Promos
+    {
+        B1, B2, B3, M1, M2
+    }
+
+    enum Promos_Init
+    {
+        B1 = 1, B2 = 2, B3 = 3, M2 = 5, M1 = 4
+    }
+}
