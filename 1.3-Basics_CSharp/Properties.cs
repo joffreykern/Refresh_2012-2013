6e7ebfa1b83379e5f5528ae89746652c36a8223e
Joffrey Kern <joffrey.kern@live.fr>
2012-10-16 09:54:13 +0200

    add properties examples

---
+++ 1.3-Basics_CSharp/Properties.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3_Basics_CSharp
+{
+    class Properties
+    {
+        private int _number;
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        void UseNumber()
+        {
+            this.Number = 1;
+            Console.WriteLine(this.Number);
+        }
+
+        public string MyString { get; set; }
+
+        void UseString()
+        {
+            this.MyString = "a string";
+            Console.WriteLine(this.MyString);
+        }
+
+    }
+}
