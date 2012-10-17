cc86de9280e9715a1829319b017199c0e6841db1
Joffrey Kern <joffrey.kern@live.fr>
2012-10-17 08:55:04 +0200

    1.5 done

--- 1.5/Program.cs
+++ 1.5/Program.cs
@@ -10,6 +10,15 @@ namespace _1._5
     {
         static void Main(string[] args)
         {
+            int integer = 0;
+            long l = integer;
+
+            double d = 0.0d;
+            int i = (int)d;
+
+            DateTime time = new DateTime(2012, 10, 17);
+            Console.WriteLine( time.ToShortDateString());
+            Console.Read();
         }
     }
 }
